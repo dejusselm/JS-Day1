c676cba8ada53ce6e1b19b1e472a8eb105efba33
dejusselm <dejusselm@gmail.com>
2026-01-08 11:21:52 +0100

    Partie 2

--- TP1/script.js
+++ TP1/script.js
@@ -3,3 +3,13 @@ let stud_number  = 28;
 let is_open = false;
 
 console.log(classe,stud_number,is_open);
+
+//*******************************************************//
+
+let student = {
+    name : "Maeva",
+    maths : 14,
+    french : 15
+};
+
+console.log(student.name);
