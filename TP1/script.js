6509d1133a2494d06056dde058d8f54ffb0f4153
dejusselm <dejusselm@gmail.com>
2026-01-08 11:52:44 +0100

    Partie 5

--- TP1/script.js
+++ TP1/script.js
@@ -1,5 +1,5 @@
 const classe = "B1A";
-let stud_number  = 28;
+let stud_number = 28;
 let is_open = false;
 
 // console.log(classe,stud_number,is_open);
@@ -7,9 +7,9 @@ let is_open = false;
 //*************************Part 2******************************//
 
 let student1 = {
-    name : "Maeva",
-    maths : 14,
-    french : 15
+    name: "Maeva",
+    maths: 14,
+    french: 15
 };
 
 // console.log(student1.name);
@@ -19,31 +19,41 @@ let student1 = {
 let group = [student1];
 
 let student2 = {
-    name : "Marine",
-    maths : 13,
-    french : 16
+    name: "Marine",
+    maths: 13,
+    french: 16
 };
 
 let student3 = {
-    name : "Marion",
-    maths : 15,
-    french : 15
+    name: "Marion",
+    maths: 15,
+    french: 15
 };
 
-group.push(student2,student3);
+group.push(student2, student3);
 
-// for (let i=0; i<=group.length-1; i++){
-//     console.log(i);
-//     console.log(group[i].name);
-// }
+/*for (let i=0; i<=group.length-1; i++){
+    console.log(i);
+    console.log(group[i].name);
+}*/
 
 //*************************Part 4******************************//
 
-for (student of group){
+/*for (student of group){
     console.log(student.name);
     let avg = (student.maths+student.french)/2;
     console.log("Average : ", avg);
-}
+}*/
 
 
 //**************************Part 5*******************************//
+
+for (student of group) {
+    let avg = (student.maths + student.french) / 2;
+    console.log(student.name);
+    if (avg>=10){
+        console.log("Admis(e)");
+    } else{
+        console.log("Refusé.e");
+    }
+}
